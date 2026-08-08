7cd024eebb752a76b40f39d73cf0bef22a5449d0
Jennifer Reynolds <jadedResearcher@gmail.com>
2026-08-08 11:30:10 -0400

    problem, thumbnail doesn't work

--- Lavinraca/index.js
+++ Lavinraca/index.js
@@ -17,6 +17,17 @@ window.onload = () => {
   }*/
 }
 
+const attachObviousExits = (contentDirectory, obviousExits) => {
+  const c = createElementWithClassAndParent("div", story);
+  c.innerHTML = "<br>Obvious Exits Are:<br><br>"
+
+  for (let exit of obviousExits) {
+    const button = createElementWithClassAndParent("button", c);
+    button.innerText = exit.text;
+    button.onclick = () => exit.function(contentDirectory);
+  }
+}
+
 /*
 you can approach the door, the harvest, the mailbox or the backyard
 content directory decides if its the normal outside or if its silly/spooky
@@ -29,12 +40,8 @@ const outsideTheHouse = (contentDirectory = "images/Diorama/Outside/Normal") => 
   obviousExits.push({ text: "Approach the Door", function: outsideTheDoor })
   obviousExits.push({ text: "Approach the Harvest", function: theHarvest })
 
-  story.innerHTML = `${text} <br><Br>Obvious Exits Are:<br><Br>`;
-  for (let exit of obviousExits) {
-    const button = createElementWithClassAndParent("button", story);
-    button.innerText = exit.text;
-    button.onclick = () => exit.function(contentDirectory);
-  }
+  story.innerHTML = `${text}`;
+  attachObviousExits(contentDirectory, obviousExits)
 }
 
 const outsideTheDoor = (contentDirectory) => {
@@ -44,11 +51,23 @@ const outsideTheDoor = (contentDirectory) => {
   video.src = contentDirectory + "/approach_door.mp4";
   storyContainer.style.display = "none"
   video.play();
+  const obviousExits = [];
+  obviousExits.push({ text: "Flee", function: outsideTheHouse })
+  obviousExits.push({ text: "Knock", function: knockOnDoor })
+
   video.onended = () => {
     storyContainer.style.display = "block"
     story.innerText = "With beating heart and shaky hands you reach the door. What wonders and horrors will you find within?"
+    attachObviousExits(contentDirectory, obviousExits)
+
   }
 }
+
+const knockOnDoor = () => {
+  alert("JR NOTE: TODO")
+}
+
+
 const theHarvest = (contentDirectory) => {
   alert("TODO: outside the harvest")
 }
